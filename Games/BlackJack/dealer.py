bbb3dd67676338dc17332b65fedf0c1c7891cbf5
Flo0205 <florian0205@gmx.de>
2022-02-22 23:49:07 +0100

    Black Jack done

--- Games/BlackJack/dealer.py
+++ Games/BlackJack/dealer.py
@@ -1,10 +1,23 @@
+from random import randint
 from playerBase import PlayerBase
 
 
 class Dealer(PlayerBase):
     def __init__(self, deck):
         super().__init__(deck)
+        self.riskRoundDone = False
 
     def DecideAction(self):
-        # TODO: Spieltaktik des Dealers überlegen und einbauen
-        pass
+        value = self.GetHandValue()
+
+        if value < 16:
+            self.Draw()
+        elif self.riskRoundDone:
+            self.Pass()
+        else:
+            # Wenn randint() ungerade ist, dann zieht der Dealer trotz hohem Risiko
+            if randint(1, 10) % 2 == 1:
+                self.Draw()
+                self.riskRoundDone = True
+            else:
+                self.Pass()
--- Games/BlackJack/table.py
+++ Games/BlackJack/table.py
@@ -1,52 +1,63 @@
+from tokenize import String
 from deck import Deck
 from player import Player
 from dealer import Dealer
 
-def main():
-    # Initialisierung der Objekte
-    cardDeck = Deck()
-    dealer = Dealer(cardDeck)
-    player = Player(cardDeck)
+class Table(object):
+    def __init__(self):
+        # Initialisierung der Objekte
+        self._cardDeck = Deck()
+        self._dealer = Dealer(self._cardDeck)
+        self._player = Player(self._cardDeck)
 
-    # Erstellung der Variablen außerhalb der Schleife
-    pValue = 0
-    dValue = 0
+        # Erstellung der Werte-Variablen
+        self.pValue = 0
+        self.dValue = 0
 
-    # Lässt das Spiel solange laufen, bis beide passen oder einer verliert
-    while not player.Done or not dealer.Done:
+    # Startet das Spiel. Mögliche Rückgabewerte: "win", "loss" oder "draw"
+    def Play(self) -> str:
+        # Lässt das Spiel solange laufen, bis beide passen oder einer verliert
+        while not self._player.Done or not self._dealer.Done:
 
-        # Lässt den Spieler entscheiden, solange dieser nicht gepasst hat
-        if not player.Done:
-            player.GetPlayerInput()
-        # Lässt den Dealer entscheiden, solange dieser nicht gepasst hat
-        if not dealer.Done:
-            dealer.DecideAction()
+            # Lässt den Spieler entscheiden, solange dieser nicht gepasst hat
+            if not self._player.Done:
+                self._player.GetPlayerInput()
+            # Lässt den Dealer entscheiden, solange dieser nicht gepasst hat
+            if not self._dealer.Done:
+                self._dealer.DecideAction()
 
-        # Laden der Handwerte
-        pValue = player.GetHandValue()
-        dValue = dealer.GetHandValue()
-        printStats(pValue, dValue)
+            # Laden der Handwerte
+            self.pValue = self._player.GetHandValue()
+            self.dValue = self._dealer.GetHandValue()
+            self._printStats(self.pValue, self.dValue)
 
-        # Check ob einer über 21 ist
-        if pValue > 21:
-            print("\nDu hast verloren!")
-        elif dValue > 21:
+            # Check ob einer über 21 ist
+            if self.pValue > 21:
+                print("\nDu hast verloren!")
+                return "win"
+            elif self.dValue > 21:
+                print("\nDu hast gewonnen!")
+                return "loss"
+
+        if self.pValue == self.dValue:
+            print("\nKeiner gewinnt!")
+            return "draw"
+        elif self.pValue > self.dValue:
             print("\nDu hast gewonnen!")
+            return "win"
+        else:
+            print("\nDu hast verloren!")
+            return "loss"
 
-    if pValue == dValue:
-        print("\nKeiner gewinnt!")
-    elif pValue > dValue:
-        print("\nDu hast gewonnen!")
-    else:
-        print("\nDu hast verloren!")
+    # Gibt die Werte beider Hände in der Konsole aus
+    def _printStats(self, pValue, dValue):
+        print("\n\n-----------------------")
+        print(f"Hand: {pValue}")
+        print(f"Dealer: {dValue}")
+        print("-----------------------\n")
 
-# Gibt die Werte beider Hände in der Konsole aus
-def printStats(pValue, dValue):
-    print("\n\n-----------------------")
-    print(f"Hand: {pValue}")
-    print(f"Dealer: {dValue}")
-    print("-----------------------\n")
 
 # Startet das Spiel, wenn diese Datei ausgeführt wird
 if __name__ == "__main__":
-    main()
+    table = Table()
+    table.Play()
